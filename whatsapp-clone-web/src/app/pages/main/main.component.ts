c5917cd7073bfe43662bae2e76bc6cd5b50ce49d
Abdellah <abdallahkaba98@gmail.com>
2025-01-16 10:35:39 +0000

    Implement the chat list component

--- whatsapp-clone-web/src/app/pages/main/main.component.ts
+++ whatsapp-clone-web/src/app/pages/main/main.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ChatService} from "../../services/services/chat.service";
+import {ChatResponse} from "../../services/models/chat-response";
+import {ChatListComponent} from "../../components/chat-list/chat-list.component";
+import {KeycloakService} from "../../utils/keycloak/keycloak.service";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [
+    ChatListComponent
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
-export class MainComponent {
+export class MainComponent implements OnInit{
+
+  chats: Array<ChatResponse> = []
+  constructor(
+    private chatService: ChatService,
+    private keycloakService: KeycloakService
+  ) {
+  }
+
+  ngOnInit() {
+    this.getAllChats();
+  }
+
+  private getAllChats(){
+    this.chatService.getChatsByReceiver()
+      .subscribe({
+        next: (res => {
+          this.chats = res
+        })
+      })
+  }
+
+  logout() {
+    this.keycloakService.logout();
+  }
+
+  userProfile() {
+    this.keycloakService.accountManagement();
+  }
 
 }

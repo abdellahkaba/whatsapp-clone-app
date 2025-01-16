bb0ded79f3cef6066cd89df80b3b4c2e7f85dc52
Abdellah <abdallahkaba98@gmail.com>
2025-01-16 07:25:54 +0000

    Keycloak configuration and configure the Http Interceptor

--- whatsapp-clone-web/src/app/app.config.ts
+++ whatsapp-clone-web/src/app/app.config.ts
@@ -1,8 +1,30 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  provideZoneChangeDetection
+}
+  from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {KeycloakService} from "./utils/keycloak/keycloak.service";
+import {keycloakHttpInterceptor} from "./utils/http/keycloak-http.interceptor";
 
+export function kcFactory(kcService: KeycloakService) {
+  return () => kcService.init();
+}
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([keycloakHttpInterceptor])
+    ),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: kcFactory,
+      deps: [KeycloakService],
+      multi: true
+    }
+  ]
 };

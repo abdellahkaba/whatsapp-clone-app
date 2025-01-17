5373f0cd7b0b9f137795e5d2f618eaac40858a70
Abdellah <abdallahkaba98@gmail.com>
2025-01-17 15:35:55 +0000

    New version for angular

---
+++ whatsapp-clone-ui/src/app/app.config.ts
@@ -0,0 +1,26 @@
+import { ApplicationConfig, provideZoneChangeDetection, inject, provideAppInitializer }
+  from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {KeycloakService} from "./utils/keycloak/keycloak.service";
+import {keycloakHttpInterceptor} from "./utils/http/keycloak-http.interceptor";
+
+export function kcFactory(kcService: KeycloakService) {
+  return () => kcService.init();
+}
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([keycloakHttpInterceptor])
+    ),
+    provideAppInitializer(() => {
+      const initFn = ((key: KeycloakService) => {
+        return () => key.init()
+      })(inject(KeycloakService));
+      return initFn();
+    })
+  ]
+};
